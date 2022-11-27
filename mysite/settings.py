1460be2d6e8521e8b0d51aaa1839af2868493fa8
Antonio Alonso <alloant@gmail.com>
2022-11-27 10:10:00 +0530

    crispy

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,6 +104,8 @@ else:
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
+DEBUG = True
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
